69744c905d8bff3a1eb61d52583ae2acbd3d80ff
VuDoanDungVN <vudungit92@gmail.com>
2024-05-08 00:56:40 +0900

    delete not function

--- app/api/department/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server';
-
-import { prisma } from '@/app/_utils/prismaSingleton';
-
-export async function GET() {
-  const departments = await prisma.department.findMany();
-  return NextResponse.json(departments, { status: 200 });
-}
--- app/api/thanks_card/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server';
-
-import { ThanksCardRepository } from '@/app/_repositories/ThanksCard';
-
-export async function GET() {
-  const thanksCards = await ThanksCardRepository.findMany();
-  return NextResponse.json(thanksCards);
-}
